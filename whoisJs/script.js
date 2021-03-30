7f9cfb4b12b3ddd65f2b9a79d4c810a97ed9c693
Mateus Santos <m.santos.azeve@gmail.com>
2021-03-30 11:56:40 -0300

    exemplos de comentarios em JS

--- whoisJs/script.js
+++ whoisJs/script.js
@@ -1,8 +1,17 @@
+// Funcao: Cria um novo paragrafo e o insere no fim do arquivo HTML
+
 function createParagraph() {
   let para = document.createElement('p');
   para.textContent = 'Você clicou no botão!';
   document.body.appendChild(para);
 }
+
+/*
+  1. Captura referencias de todos os botoes na pagina e armazena isso em um array.
+  2. Vai ate todos os botoes e adiciona um event listener click a cada um deles.
+
+  Quando cada botao e clicado, a funcao createParagraph() sera executada
+*/
 
 const buttons = document.querySelectorAll('button');
 
