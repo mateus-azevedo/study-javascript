feec8b2be18a231a2a2df6503693a14969291953
Mateus Santos <m.santos.azeve@gmail.com>
2021-03-31 07:43:11 -0300

    estudando variaveis

---
+++ variables/script.js
@@ -0,0 +1,30 @@
+// Criando variaveis
+var meuNome;
+//atribuindo
+meuNome = 'Chris';
+
+function logNome() {
+  console.log(meuNome);
+}
+
+
+              /* Tipos de Variaveis */ 
+// Numero
+var minhaIdade = 17;
+
+// String
+var despedidaGolfinho = 'Até logo e obrigado por todos os peixes!';
+
+// Booleans
+var estouVivo = true;
+var teste = 6 < 3;
+
+// Arrays
+var meuNomeArray = ['Chris', 'Bob', 'Jim'];
+var meuNumeroArray = [10, 15, 40];
+
+console.log(`Meu nome é ${meuNomeArray[0]} e tenho ${meuNumeroArray[2]} anos de idade`);
+
+// Objetos
+var cachorro = { nome: 'Totó', raca: 'Dálmata' };
+console.log(cachorro.nome);
