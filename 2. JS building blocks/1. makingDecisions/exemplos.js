2f264626c5fb35945c71622c469a1d438d928d61
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-02 13:35:18 -0300

    explicacao de como utilizar if

---
+++ 2. JS building blocks/1. makingDecisions/exemplos.js
@@ -0,0 +1,27 @@
+/* MODE DE UTILIZAR O IF*/
+/* 1 MODO */
+if (condicao) {
+  // codigo para executar caso a condicao seja verdadeira
+} else {
+  // senao, executar este codigo
+}
+
+
+/* 2 MODO */
+if (condicao) {
+  // condigo para executar caso a condicao seja verdadeira
+}
+// codigo a ser executado
+
+
+/* 3 MODO */
+if (condicao) //executa aqui se for verdadeira
+else //executa este outro codigo
+
+
+/* EXEMPLO REAL */
+if(comprasFeitas === true) {
+  var granaFilhote = 10;
+} else {
+  var granaFilhote = 5;
+}
