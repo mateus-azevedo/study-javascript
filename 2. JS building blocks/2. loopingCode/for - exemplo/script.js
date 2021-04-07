7bed39b69d56a89d4c9fdaee4999fa44803a24de
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-07 18:01:32 -0300

    exemplo de uso do for

---
+++ 2. JS building blocks/2. loopingCode/for - exemplo/script.js
@@ -0,0 +1,13 @@
+const cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+let info = 'My cats are called ';
+const para = document.querySelector('p');
+
+for (let i = 0; i < cats.length; i++) {
+  if (i === cats.length - 1) {
+    info += 'and ' + cats[i] + '.';
+  } else {
+    info += cats[i] + ', ';
+  }
+}
+
+para.textContent = info;
