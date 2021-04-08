1d0395f5d697eca8bcb398610274af40a79c4034
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-08 18:01:34 -0300

    exemplo de uso while

---
+++ 2. JS building blocks/2. loopingCode/while - exemplo/script.js
@@ -0,0 +1,17 @@
+const cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+let info = 'My cats are called ';
+const para = document.querySelector('p');
+
+let i = 0
+
+while (i < cats.length) {
+  if (i === cats.length - 1) {
+    info += 'and ' + cats[i] + '.';
+  } else {
+    info += cats[i] + ', ';
+  }
+
+  i++;
+}
+
+para.textContent = info;
